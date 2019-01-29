4de1c7bc493cae0b9cb37de59f3040e118458e0a
aamir <wasi3aamir@gmail.com>
2019-01-29 19:19:20 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                long sum = 0;
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                long num = long.Parse(space[0]);
+                sum = briangle(num - 1);
+                Console.WriteLine(sum);
+            }
+        }
+        public static long briangle(long n)
+        {
+            long rem;
+            rem = (long)(2 * Math.Pow(n, 2));
+            return rem;
+        }
+    }
+}
